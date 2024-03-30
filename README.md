e534385c6c09aacccad49592595fa317858649d7
renzaspiras <aspirasrenz@gmail.com>
2024-03-30 22:28:14 +0800

    alpha 0

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# offline-ai
+# Offline-GPT
+
+Are you tired of being without internet access when you need to search for information? Do you desire an AI companion that respects your privacy and operates solely for you? Look no further than **Offline-GPT**.
+
---
+++ install.sh
@@ -0,0 +1,14 @@
+mkdir kvm
+pacman -S qemu qemu-utils qemu-common qemu-system-x86_64-headless --noconfirm
+qemu-img create -f qcow2 alpine.img 5G
+wget  wget http://dl-cdn.alpinelinux.org/alpine/v3.12/releases/x86_64/alpine-virt-3.12.3-x86_64.iso
+
+mv *.img ./kvm/
+mv *.iso ./kvm/os.iso
+
+
+qemu-system-x86_64 -machine q35 -m 1024 -smp cpus=2 -cpu qemu64 \
+  -drive if=pflash,format=raw,read-only,file=$PREFIX/share/qemu/edk2-x86_64-code.fd \
+  -netdev user,id=n1,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 \
+  -cdrom ./kvm/os.iso \
+  -nographic ./kvm/alpine.img
