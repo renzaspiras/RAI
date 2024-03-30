f4a7bfa3c51d2c50a1548499e046715287f6ae46
renzaspiras <aspirasrenz@gmail.com>
2024-03-30 22:58:01 +0800

    no graphic

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ wget https://dl-cdn.alpinelinux.org/alpine/v3.19/releases/aarch64/alpine-virt-3.
 mv *.img ./kvm/
 mv *.iso ./kvm/os.iso
 
-qemu-system-aarch64 -M virt -cpu cortex-a57 -m 1024 -drive format=raw,file=./kvm/alpine.img -cdrom ./kvm/os.iso
+qemu-system-aarch64 -M virt -cpu cortex-a57 -m 1024 -drive format=raw,file=./kvm/alpine.img -cdrom ./kvm/os.iso -nographic
