393367a976af04d816026cdd5c127557d1784726
renzaspiras <aspirasrenz@gmail.com>
2024-03-30 22:44:50 +0800

    nographic

--- install.sh
+++ install.sh
@@ -6,9 +6,10 @@ wget  wget https://dl-cdn.alpinelinux.org/alpine/v3.19/releases/aarch64/alpine-v
 mv *.img ./kvm/
 mv *.iso ./kvm/os.iso
 
-
-qemu-system-aarch64 -machine q35 -m 1024 -smp cpus=2 -cpu qemu64 \
-  -drive if=pflash,format=raw,read-only,file=$PREFIX/share/qemu/edk2-x86_64-code.fd \
-  -netdev user,id=n1,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 \
+qemu-system-aarch64 -machine virt,accel=kvm -cpu cortex-a53 -m 1024 \
+  -drive if=pflash,format=raw,file=$PREFIX/share/qemu/edk2-aarch64-code.fd,readonly \
+  -drive file=./kvm/alpine.img,format=raw \
+  -device virtio-net-pci,netdev=n1 \
+  -netdev user,id=n1,hostfwd=tcp::2222-:22 \
   -cdrom ./kvm/os.iso \
-  -nographic ./kvm/alpine.img
+  -nographic
